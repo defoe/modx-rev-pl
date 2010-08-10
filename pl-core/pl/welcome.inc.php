749205cf891c641ce1c586eeb2f5e995e48b5a9f
qcol <info@probit.net>
2010-08-10 00:43:31 -0700

    uzupełnienie komentarzami z oryginalnymi odpowiednikami, dokończenie pliku

--- pl-core/pl/welcome.inc.php
+++ pl-core/pl/welcome.inc.php
@@ -6,12 +6,12 @@
  * @package modx
  * @subpackage lexicon
  */
-$_lang['modx_news'] = 'Aktualności MODx';
-$_lang['security_notices'] = 'Informacje dotyczące bezpieczeństwa';
-$_lang['welcome_messages'] = 'Twoja skrzynka zawiera <strong>%d</strong> wiadomości, z których <strong>%s</strong> są nieprzeczytane.';
-$_lang['welcome_title'] = 'Welcome to your MODx Content Manager';
-$_lang['yourinfo_message'] = 'This section shows some information about you:';
-$_lang['yourinfo_previous_login'] = 'Your last login:';
-$_lang['yourinfo_title'] = 'Your info';
-$_lang['yourinfo_total_logins'] = 'Total number of logins:';
-$_lang['yourinfo_username'] = 'You are logged in as:';
+$_lang['modx_news'] = 'Aktualności MODx'; //MODx News
+$_lang['security_notices'] = 'Informacje dotyczące bezpieczeństwa'; //Security Notices
+$_lang['welcome_messages'] = 'Twoja skrzynka zawiera <strong>%d</strong> wiadomości, z których <strong>%s</strong> są nieprzeczytane.'; //Your Inbox contains <strong>%d</strong> message(s), of which <strong>%s</strong> are unread.
+$_lang['welcome_title'] = 'Witaj w MODx CMS'; //Welcome to your MODx Content Manager
+$_lang['yourinfo_message'] = 'Tutaj trochę informacji o Tobie:'; //This section shows some information about you:
+$_lang['yourinfo_previous_login'] = 'Ostatnie logowanie:'; //Your last login
+$_lang['yourinfo_title'] = 'Informacje'; //Your info
+$_lang['yourinfo_total_logins'] = 'Ogólna liczba logowań:'; //Total number of logins
+$_lang['yourinfo_username'] = 'Jesteś zalogowany jako:'; //You are logged in as
